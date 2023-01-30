ebec59d17cd1dbc749adb143761aec7c55382329
Herbert Lim <mogulist@naver.com>
2023-01-30 16:59:19 +0900

    Refactor module and controller [lecture 8]

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,5 @@
-import { Controller, Module, Get } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-
-@Controller()
-class AppController {
-  // ...
-  @Get()
-  getRootRoute() {
-    return "Hello World!";
-  }
-}
-
-@Module({
-  controllers: [AppController],
-})
-class AppModule {
-  // ...
-}
+import { AppModule } from "./AppModule";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
